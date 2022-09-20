a08282ad433c4bdb3346b10ba87dbee94b839272
Volodymyr <volodymyr.liubashenko@altexsoft.com>
2022-09-20 14:07:42 +0300

    fix footer android

--- components/Footer/Footer.styled.tsx
+++ components/Footer/Footer.styled.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
+  position: relative;
   width: 100%;
   height: 65px;
   padding: 10px 20px;
+  z-index: 1;
   ul {
     display: flex;
     justify-content: flex-end;
