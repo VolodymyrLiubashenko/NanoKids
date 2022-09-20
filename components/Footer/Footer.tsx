1c7beb9b170de9ca34ced696d46e54c75bf71eb9
Volodymyr <volodymyr.liubashenko@altexsoft.com>
2022-09-20 13:03:39 +0300

    add footer links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,24 +1,31 @@
 import {StyledLinkButton} from 'components/Button/Button.styled';
+import CustomLink from 'components/Link/Link';
 import Image from 'next/image';
 import {StyledFooter} from './Footer.styled';
 
 interface FooterPropsInterface {}
-const contacts = ['viber', 'telegram', 'instagram', 'phone'];
+
+const contacts = [
+  {name: 'viber', href: 'tel:+380131226'},
+  {name: 'telegram', href: 'https://t.me/nanokidsbest'},
+  {name: 'instagram', href: 'https://www.instagram.com/nanokids_odessa/'},
+  {name: 'phone', href: 'tel:+380131226'},
+];
 
 const Footer: React.FC<FooterPropsInterface> = () => {
   return (
     <StyledFooter>
       <ul>
         {contacts.map((el) => (
-          <li key={el}>
-            <StyledLinkButton>
+          <li key={el.name}>
+            <CustomLink href={el.href}>
               <Image
-                src={`/images/${el}.svg`}
+                src={`/images/${el.name}.svg`}
                 width={30}
                 height={30}
-                alt={el}
+                alt={el.name}
               />
-            </StyledLinkButton>
+            </CustomLink>
           </li>
         ))}
       </ul>
