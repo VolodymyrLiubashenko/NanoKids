1c56d4605a85f8fa526ad71d3110709433fee4f9
Volodymyr <volodymyr.liubashenko@altexsoft.com>
2022-10-04 18:54:41 +0300

    test

--- api/newsApi.ts
+++ api/newsApi.ts
@@ -7,7 +7,14 @@ interface NewsApiInterface {
 const newsApi: NewsApiInterface = {
   getNews: async () => {
     try {
-      const result = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/news`);
+      const result = await fetch(
+        `${process.env.NEXT_PUBLIC_BASEURL}/api/news`,
+        {
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+          },
+        }
+      );
 
       const news = await result.json();
       return news;
