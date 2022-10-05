5d90516425e61a9c50022824997a42a56e8113d9
Volodymyr <volodymyr.liubashenko@altexsoft.com>
2022-10-05 11:40:59 +0300

    test10

--- pages/api/players.ts
+++ pages/api/players.ts
@@ -1,63 +1,12 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import fs from 'fs/promises';
 
-const p = [
-  {
-    firstName: 'Vova',
-    lastName: 'Liubashenko',
-    tshirtNumber: 10,
-    position: 'forward',
-    team: 'firstTeam',
-  },
-  {
-    firstName: 'Vova',
-    lastName: 'Liubashenko',
-    tshirtNumber: 17,
-    position: 'forward',
-    team: 'firstTeam',
-  },
-  {
-    firstName: 'Yarik',
-    lastName: 'Liubashenko',
-    tshirtNumber: 11,
-    position: 'forward',
-    team: 'firstTeam',
-  },
-  {
-    firstName: 'Andrew',
-    lastName: 'Liubashenko',
-    tshirtNumber: 12,
-    position: 'forward',
-    team: 'firstTeam',
-  },
-  {
-    firstName: 'Dima',
-    lastName: 'Liubashenko',
-    tshirtNumber: 13,
-    position: 'forward',
-    team: 'secondTeam',
-  },
-  {
-    firstName: 'Valera',
-    lastName: 'Liubashenko',
-    tshirtNumber: 14,
-    position: 'forward',
-    team: 'secondTeam',
-  },
-  {
-    firstName: 'Semen',
-    lastName: 'Liubashenko',
-    tshirtNumber: 15,
-    position: 'forward',
-    team: 'secondTeam',
-  },
-];
-
-export default async function handleGetPlayers(
+export default async function handleGetNews(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const rawData = await fs.readFile('db/players.json', 'utf8');
-  const result = await JSON.parse(rawData);
-  res.json(p);
+  const players = await JSON.parse(rawData);
+
+  res.json(players);
 }
--- pages/api/test.ts
+++
@@ -1,5 +0,0 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
-
-export default function test(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({test: 'test'});
-}
