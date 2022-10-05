6db88191acb26ecd65e095f60d44fb6d0116a49a
Volodymyr <volodymyr.liubashenko@altexsoft.com>
2022-10-05 09:46:44 +0300

    test3

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -1,12 +1,12 @@
 import {NextApiRequest, NextApiResponse} from 'next';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 export default async function getNews(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const rawData = fs.readFileSync('db/news.json', 'utf8');
-  const news = JSON.parse(rawData);
+  const rawData = await fs.readFile('db/news.json', 'utf8');
+  const news = await JSON.parse(rawData);
 
   res.json(news);
 }
